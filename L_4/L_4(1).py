be6fde610244a7526913aad567ec885beb5c4aaf
AnnSay <79140444+AnnSay@users.noreply.github.com>
2021-03-12 20:56:41 +0300

    Add files via upload

---
+++ L_4/L_4(1).py
@@ -0,0 +1,16 @@
+"""Реализовать скрипт, в котором должна быть предусмотрена функция расчёта заработной платы сотрудника.
+Используйте в нём формулу: (выработка в часах*ставка в час) + премия.
+Во время выполнения расчёта для конкретных значений необходимо запускать скрипт с парамет"""
+
+from sys import argv
+
+
+def salary():
+    try:
+        time, rate, bonus = map(float, argv[1:])
+        print(f'Salary -{time * rate + bonus}')
+    except ValueError:
+        print('Enter all 3 numbers. Not string or empy character.')
+
+
+salary()
---
+++ L_4/L_4(5).py
@@ -0,0 +1,14 @@
+"""Реализовать формирование списка, используя функцию range() и возможности генератора. В список должны войти чётные числа
+от 100 до 1000 (включая границы). Нужно получить результат вычисления произведения всех элементов списка.
+Подсказка: использовать функцию reduce().
+"""
+
+from functools import reduce
+
+
+def mul_list(el_1, el_2):
+    return el_1 * el_2
+
+
+uniq_list = [el for el in range(100, 1001, 2)]
+print(f'List\n{uniq_list}\nMultiplication of numbers\n{reduce(mul_list, uniq_list)}')
